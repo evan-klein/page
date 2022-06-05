aa9279bdc50c8f4ce8bc774cfc4124c5091892f2
Evan Klein <me@evanklein.com>
2022-06-05 11:14:42 -0700

    Progress commit; squash later

---
+++ Page.php
@@ -0,0 +1,21 @@
+<?php
+
+class Page {
+	public function __construct(){
+		// TODO
+
+		return $this;
+	}
+
+
+	public function head(){
+		// TODO
+	}
+
+
+	public function tail(){
+		// TODO
+	}
+}
+
+?>
